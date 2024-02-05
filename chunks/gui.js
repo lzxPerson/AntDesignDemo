1c4f4f9f63df81b5ebd4dab92539b7cb269e5e28
lzxPerson <13460178108@163.com>
2024-02-05 19:13:27 +0800

    123

---
+++ chunks/gui.js
@@ -0,0 +1,232 @@
+var GUI =
+(window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([["gui"],{
+
+/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/index.css":
+/*!*****************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--5-1!./node_modules/postcss-loader/src??postcss!./src/playground/index.css ***!
+  \*****************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "html,\nbody,\n.index_app_2mqDO {\n    /* probably unecessary, transitional until layout is refactored */\n    width: 100%; \n    height: 100%;\n    margin: 0;\n\n    /* Setting min height/width makes the UI scroll below those sizes */\n    min-width: 1024px;\n    min-height: 640px; /* Min height to fit sprite/backdrop button */\n}\n\n/* @todo: move globally? Safe / side FX, for blocks particularly? */\n\n* { -webkit-box-sizing: border-box; box-sizing: border-box; }\n", ""]);
+
+// exports
+exports.locals = {
+	"app": "index_app_2mqDO"
+};
+
+/***/ }),
+
+/***/ "./src/playground/index.css":
+/*!**********************************!*\
+  !*** ./src/playground/index.css ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../node_modules/css-loader??ref--5-1!../../node_modules/postcss-loader/src??postcss!./index.css */ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/playground/index.css");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
+/***/ "./src/playground/index.jsx":
+/*!**********************************!*\
+  !*** ./src/playground/index.jsx ***!
+  \**********************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! es6-object-assign/auto */ "./node_modules/es6-object-assign/auto.js");
+/* harmony import */ var es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! core-js/fn/array/includes */ "./node_modules/core-js/fn/array/includes.js");
+/* harmony import */ var core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! core-js/fn/promise/finally */ "./node_modules/core-js/fn/promise/finally.js");
+/* harmony import */ var core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var intl__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! intl */ "./node_modules/intl/index.js");
+/* harmony import */ var intl__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(intl__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var _lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
+/* harmony import */ var _components_browser_modal_browser_modal_jsx__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../components/browser-modal/browser-modal.jsx */ "./src/components/browser-modal/browser-modal.jsx");
+/* harmony import */ var _lib_supported_browser__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../lib/supported-browser */ "./src/lib/supported-browser.js");
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./index.css */ "./src/playground/index.css");
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_9__);
+// 多功能填充
+
+
+
+ // For Safari 9
+
+
+
+//引入 提供重构状态的高阶组件
+
+// 浏览器不支持下提示组件
+
+//辅助函数，用于确定浏览器是否受支持。
+
+
+var appTarget = document.createElement('div');
+appTarget.className = _index_css__WEBPACK_IMPORTED_MODULE_9___default.a.app;
+console.log("18", _index_css__WEBPACK_IMPORTED_MODULE_9___default.a);
+document.body.appendChild(appTarget);
+if (Object(_lib_supported_browser__WEBPACK_IMPORTED_MODULE_8__["default"])()) {
+  console.log("132", appTarget);
+  debugger;
+  // require needed here to avoid importing unsupported browser-crashing code
+  // at the top level
+  __webpack_require__(/*! ./render-gui.jsx */ "./src/playground/render-gui.jsx").default(appTarget);
+} else {
+  _components_browser_modal_browser_modal_jsx__WEBPACK_IMPORTED_MODULE_7__["default"].setAppElement(appTarget);
+  var WrappedBrowserModalComponent = Object(_lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_6__["default"])(_components_browser_modal_browser_modal_jsx__WEBPACK_IMPORTED_MODULE_7__["default"], true /* localesOnly */);
+  var handleBack = function handleBack() {};
+  // eslint-disable-next-line react/jsx-no-bind
+  react_dom__WEBPACK_IMPORTED_MODULE_5___default.a.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_4___default.a.createElement(WrappedBrowserModalComponent, {
+    onBack: handleBack
+  }), appTarget);
+}
+
+/***/ }),
+
+/***/ "./src/playground/render-gui.jsx":
+/*!***************************************!*\
+  !*** ./src/playground/render-gui.jsx ***!
+  \***************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! redux */ "./node_modules/redux/es/index.js");
+/* harmony import */ var _lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../lib/app-state-hoc.jsx */ "./src/lib/app-state-hoc.jsx");
+/* harmony import */ var _containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../containers/gui.jsx */ "./src/containers/gui.jsx");
+/* harmony import */ var _lib_hash_parser_hoc_jsx__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../lib/hash-parser-hoc.jsx */ "./src/lib/hash-parser-hoc.jsx");
+/* harmony import */ var _lib_log_js__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../lib/log.js */ "./src/lib/log.js");
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+
+
+
+
+
+
+var onClickLogo = function onClickLogo() {
+  console.log("我点击了网站logo");
+  // window.location = 'https://scratch.mit.edu';
+};
+
+var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
+  Object(_lib_log_js__WEBPACK_IMPORTED_MODULE_6__["default"])('User canceled telemetry modal');
+};
+var handleTelemetryModalOptIn = function handleTelemetryModalOptIn() {
+  Object(_lib_log_js__WEBPACK_IMPORTED_MODULE_6__["default"])('User opted into telemetry');
+};
+var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
+  Object(_lib_log_js__WEBPACK_IMPORTED_MODULE_6__["default"])('User opted out of telemetry');
+};
+
+/*
+渲染 GUI 游戏场地。这是一个单独的函数，因为导入任何
+ * 会导致不支持的浏览器崩溃。
+ * {object} appTarget - 要渲染的 DOM 元素
+ * Render the GUI playground. This is a separate function because importing anything
+ * that instantiates the VM causes unsupported browsers to crash
+ * {object} appTarget - the DOM element to render to
+ */
+/* harmony default export */ __webpack_exports__["default"] = (function (appTarget) {
+  _containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__["default"].setAppElement(appTarget);
+
+  // 请注意，redux 的 "compose "函数只是作为一个通用工具使用，以使
+  // HOC 构造函数调用的层次结构更加清晰；它与 redux 的
+  // 编译减速器的能力无关。
+  // note that redux's 'compose' function is just being used as a general utility to make
+  // the hierarchy of HOC constructor calls clearer here; it has nothing to do with redux's
+  // ability to compose reducers.
+  var WrappedGui = Object(redux__WEBPACK_IMPORTED_MODULE_2__["compose"])(_lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_3__["default"], _lib_hash_parser_hoc_jsx__WEBPACK_IMPORTED_MODULE_5__["default"])(_containers_gui_jsx__WEBPACK_IMPORTED_MODULE_4__["default"]);
+
+  // TODO 测试背包的黑客程序，允许通过 url 参数设置背包主机
+  // TODO a hack for testing the backpack, allow backpack host to be set by url param
+  var backpackHostMatches = window.location.href.match(/[?&]backpack_host=([^&]*)&?/);
+  var backpackHost = backpackHostMatches ? backpackHostMatches[1] : null;
+  var scratchDesktopMatches = window.location.href.match(/[?&]isScratchDesktop=([^&]+)/);
+  var simulateScratchDesktop;
+  if (scratchDesktopMatches) {
+    try {
+      // parse 'true' into `true`, 'false' into `false`, etc.
+      simulateScratchDesktop = JSON.parse(scratchDesktopMatches[1]);
+    } catch (_unused) {
+      // it's not JSON so just use the string
+      // note that a typo like "falsy" will be treated as true
+      simulateScratchDesktop = scratchDesktopMatches[1];
+    }
+  }
+  if (false) {}
+  console.log("71", simulateScratchDesktop);
+  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(
+  // 重要： 这是检查 `simulateScratchDesktop` 是否真实，而不仅仅是定义！
+  // important: this is checking whether `simulateScratchDesktop` is truthy, not just defined!
+
+  simulateScratchDesktop ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
+    canEditTitle: true,
+    isScratchDesktop: true,
+    showTelemetryModal: true,
+    canSave: false,
+    onTelemetryModalCancel: handleTelemetryModalCancel,
+    onTelemetryModalOptIn: handleTelemetryModalOptIn,
+    onTelemetryModalOptOut: handleTelemetryModalOptOut
+  }) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
+    canEditTitle: true,
+    backpackVisible: true,
+    showComingSoon: true,
+    backpackHost: backpackHost,
+    canSave: false,
+    onClickLogo: onClickLogo
+  }), appTarget);
+});
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************!*\
+  !*** ./locale-data/complete.js (ignored) ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* (ignored) */
+
+/***/ })
+
+},[["./src/playground/index.jsx","lib.min"]]]);
+//# sourceMappingURL=gui.js.map
